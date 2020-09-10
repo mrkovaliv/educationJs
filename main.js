836a4ba735cb25400869ef7989d5c8c0c98150cb
Volodymyr <kovalivvova22@gmail.com>
2020-09-10 22:22:26 +0300

    Task1

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 'use strict';
 
-console.log(av);
-const av = 2;
+let money = +prompt("Ваш бюджет на месяц", 1000);
+let time = prompt("Введите дату в формате YYYY-MM-DD", "2020-09-10");
+let q1 = prompt("Введите обязательную статью расходов в этом месяце", "5");
+let q2 = prompt("Во сколько обойдется?", "2020");
+let appData = {
+    budjet: money,
+    timeData: time,
+    expences: {
+        [q1]: q2
+
+    },
+    optionalExpenses: {},
+    income: [],
+    savings: false
+
+};
+alert(appData.budjet/30);
+alert(appData.expences[q1]);
+
